4ec32e456da0f0031eeaa86193b8162bac2e758b
Elaine Nguyen (rbo748) <rbo748@mail.usask.ca>
2025-03-10 00:58:23 -0600

    pt2 of filtering goals 🔥

--- src/main/java/com/example/cmpt370project/GoalModel.java
+++ src/main/java/com/example/cmpt370project/GoalModel.java
@@ -1,3 +1,4 @@
+
 package com.example.cmpt370project;
 
 import java.io.*;
@@ -199,7 +200,27 @@ public class GoalModel {
     }
 
     /**
-     * Notify th subscribers of this model that the data has changed.
+     * Returns a list of goals filtered by difficulty.
+     * @param difficulty the difficulty level to filter by (e.g., "Easy", "Medium", "Hard").
+     *                   If "All" is passed, it returns all goals.
+     * @return list of goals that match the given difficulty.
+     */
+    public List<Goal> getGoalsByDifficulty(String difficulty) {
+        if (difficulty.equalsIgnoreCase("All")) {
+            return getGoals();
+        }
+        List<Goal> filteredGoals = new ArrayList<>();
+        for (Goal goal : goals.values()) {
+            if (goal.getDifficulty() != null && goal.getDifficulty().equalsIgnoreCase(difficulty)) {
+                filteredGoals.add(goal);
+            }
+        }
+        return filteredGoals;
+    }
+
+
+    /**
+     * Notify the subscribers of this model that the data has changed.
      */
     public void notifySubscribers() {
         subscribers.forEach(Subscriber::modelUpdated);
@@ -244,3 +265,4 @@ public class GoalModel {
     }
 
 }
+
