a83f745b964144b733aca65bdde5d38187d731fc
Your Name <ich524@tuxworld.usask.ca>
2025-03-11 17:38:14 -0600

    currently working on my UserProgressHistoryVisView implementation - added the model and the UserHistoryProgressVisuals page where the goal visualizations will be kept track of

---
+++ src/main/java/com/example/cmpt370project/UserHIstoryProgressVisuals.java
@@ -0,0 +1,9 @@
+package com.example.cmpt370project;
+
+/**
+ * This class focuses on creating the pie charts, line charts and scatter charts everytime the user picks either one or
+ * two of those or decides to see all of them together.
+ */
+public class UserHIstoryProgressVisuals {
+
+}
--- src/main/java/com/example/cmpt370project/UserProgressHistoryVisView.java
+++ src/main/java/com/example/cmpt370project/UserProgressHistoryVisView.java
@@ -1,4 +1,36 @@
 package com.example.cmpt370project;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
-public class UserProgressHistoryVisView {
+/**
+ * The class focuses on creating a historical view of the goals for the users. It allows them to have a visualization
+ * view with a given start date and end date.
+ */
+public class UserProgressHistoryVisView extends StackPane implements Subscriber{
+    /**
+     * The model where the data is being retrieved from.
+     */
+    private GoalModel goalModel;
+
+    /**
+     * The view where the pie charts, line charts, scatter plots visualization.
+     */
+    private UserHIstoryProgressVisuals historicalChartProgress;
+
+    /**
+     * Create a historical visualization representation page
+     */
+    public UserProgressHistoryVisView(GoalModel goalModel){
+
+    }
+
+    /**
+     * This will update the view everytime the model is updated.
+     */
+    @Override
+    public void modelUpdated() {}
+
 }
