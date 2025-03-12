cc7ef122fc42b848477e2312a1b4dba54b25bf3f
Your Name <ich524@tuxworld.usask.ca>
2025-03-11 19:36:38 -0600

    added all the attributes needed for UserHistoryProgressVisuals.java

--- src/main/java/com/example/cmpt370project/UserHIstoryProgressVisuals.java
+++ src/main/java/com/example/cmpt370project/UserHIstoryProgressVisuals.java
@@ -4,17 +4,50 @@ import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 
 /**
  * This class focuses on creating the pie charts, line charts and scatter charts everytime the user picks either one or
  * two of those or decides to see all of them together.
  */
 public class UserHIstoryProgressVisuals extends Node {
+    /**
+     * The UserProgressHIstoryVisModel is the model that consists of the pieces of data that we need for this view.
+     */
     private UserProgressHIstoryVisModel historicalChartModel;
 
-    private String GraphChoices[] = {"Line Graph", "Scatter Graph", "Pie Chart"};
+    /**
+     * CheckBox to select which graphs do the user want to view.
+     * This could be one, two or more than two graphs.
+     */
+    private CheckBox chartSelector; //no parameters needed
+
+    /**
+     * Radio Button to select if the user wants descriptive statistics to be
+     * created alongside the graphs or not.
+     */
+    private RadioButton includeDescriptiveStatistics;
+
+    /**
+     * Radio Button to select to allow the user to choose the color of their
+     * graphs.
+     */
+    private RadioButton colorPreference;
+
+    /**
+     * Piechart for the visualization purposes.
+     */
     private PieChart pieChart;
+
+    /**
+     * Linechart for the visualization purposes.
+     */
     private LineChart<String, Integer> lineChart;
+
+    /**
+     * Scatter chart for the visualization purposes.
+     */
     private ScatterChart<String, Integer> scatterChart;
 
     public UserHIstoryProgressVisuals(UserProgressHIstoryVisModel historicalChartModel){
